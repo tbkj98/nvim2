68732c24634585bfe0479e7cdc8439ec5029402d
Anand Sharma <anandsharma334@gmail.com>
2025-03-05 20:18:24 +0530

    Updated Gitsign color and symbols

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,13 +2,31 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {
+      signs = {
+        add = { text = "+" },
+        delete = { text = "-" },
+        change = { text = "*" },
+        untracked = { text = "!" },
+        topdelete = { text = "^" },
+        changedelete = { text = "~" },
+      },
       attach_to_untracked = true,
       current_line_blame_opts = {
         virt_text = true,
-        delay = 500,
+        delay = 100,
       },
     },
-    config = true,
+    config = function(_, opts)
+      require("gitsigns").setup(opts)
+
+      -- Setting highlighting groups to change the colors
+      vim.api.nvim_set_hl(0, "GitsignsAdd", { fg = "#00FF00" })
+      vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#FF0000" })
+      vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#FFA500" })
+      vim.api.nvim_set_hl(0, "GitSignsUntracked", { fg = "#00FA00" })
+      vim.api.nvim_set_hl(0, "GitSignsTopdelete", { fg = "#FA0000" })
+      vim.api.nvim_set_hl(0, "GitSignsChangedelete", { fg = "#FA0000" })
+    end,
   },
   {
     "NeogitOrg/neogit",
